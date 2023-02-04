bd5d041ef65fb1f985511c0e80d2931ce06100a4
Saumya Vatsa <saumyavatsa2108@gmail.com>
2023-02-04 17:31:15 +0530

    About showcase partially completed

---
+++ details.js
@@ -0,0 +1,7 @@
+const name = " Saumya Vatsa";
+const about = "I am a 20 year old passionate and enthusiastic about learning and exploring new adventures. Currently persuing BCA and BS (Data Science) from VIT, Vellore and IIT Madras respectively.";
+
+module.exports = {
+    name: name,
+    about : about
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Requirements
 const express = require('express')
 const ejs = require('ejs')
+const details = require("./details")
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,7 +11,9 @@ app.use(express.static("public"));
 
 
 app.get("/", (req,res)=>{
-    res.render('body')
+    res.render('body',{
+        aboutDetail: details.about
+    })
 })
 
 app.listen(port,()=>{
