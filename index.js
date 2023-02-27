dc1d10ad63e50fdb1a242315d8dcd2d49323ee10
Saumya Vatsa <saumyavatsa2108@gmail.com>
2023-02-27 11:05:57 +0530

    Changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ejs = require('ejs')
 const myRoutes = require('./Routers/routes')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 
 const app = express();
@@ -11,12 +12,13 @@ const port = process.env.PORT || 3000;
 
 // Mongo DB Connection
 mongoose.set('strictQuery', false)
-mongoose.connect("mongodb://0.0.0.0:27017/portfolioDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://saumyavatsa2108:DZ9Bib4GKzqlVMvI@portfoliodb.iolobby.mongodb.net/portfolioDB", {useNewUrlParser: true});
 
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',myRoutes);
+app.use(cors())
 
 
 app.listen(port,()=>{
