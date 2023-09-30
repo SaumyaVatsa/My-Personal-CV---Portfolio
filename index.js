50eb0da6da6eb4cef397ebef305b3fa16bfd58d4
Saumya Vatsa <saumyavatsa2108@gmail.com>
2023-09-30 20:16:40 +0530

    Final Commit

--- index.js
+++ index.js
@@ -13,15 +13,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Mongo DB Connection
-mongoose.connect('mongodb://127.0.0.1:27017/portfolio');
+mongoose.connect(process.env.CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+    ()=>{
+        console.log('Connected to Mongo Db Cloud');
+    }
+).catch((e)=>{
+    console.log(e);
+})
 
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/',myRoutes);
-// app.use(cors({
-//     origin: ["https://saumyavatsa.onrender.com/", "https://localhost:3000"]
-// }))
+app.use(cors({
+    origin: ["https://saumyavatsa.netlify.app", "https://localhost:3000"]
+}))
 
 
 app.listen(port,()=>{
